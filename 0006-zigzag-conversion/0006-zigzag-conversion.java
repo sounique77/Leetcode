d13723458756fe265087fb8661f5886ff8b2ea8d
Angelica Vancea <121234311+sounique77@users.noreply.github.com>
2023-07-07 13:44:05 +0100

    Time: 3 ms (96.89%), Space: 43.6 MB (83.27%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,25 @@
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        } else {
+            StringBuilder result = new StringBuilder();
+            int n = s.length();
+            int cycleLen = 2 * numRows - 2;
+
+            for (int i = 0; i < numRows; i++) {
+                for (int j = 0; j + i < n; j += cycleLen) {
+                    result.append(s.charAt(j + i));
+                    if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) {
+                        result.append(s.charAt(j + cycleLen - i));
+                    }
+                }
+            }
+
+            return result.toString();
+        }
+    }
+}
